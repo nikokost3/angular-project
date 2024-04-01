f51851e6586893ecdfcc8fb88d8431bb18b141c6
nikokost3 <nikoskos26@yahoo.gr>
2024-04-01 17:13:09 +0300

    new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'Nikos';
 
-  person = {
-    givenName: 'Nikos',
-    surName: 'Kostakis',
-    age: '31',
-    email: 'nikoskos@aueb.gr'
-  }
 }
