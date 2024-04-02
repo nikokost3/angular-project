efa56a4aab91626fe2c5d55fc917f3fd682c5140
nikokost3 <nikoskos26@yahoo.gr>
2024-04-02 19:00:49 +0300

    simple datatable

--- src/app/components/simple-datatable/simple-datatable.component.ts
+++ src/app/components/simple-datatable/simple-datatable.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { EPerson } from 'src/app/shared/interface/person';
+import { sortBy } from 'lodash-es';
 
 @Component({
   selector: 'app-simple-datatable',
@@ -9,5 +10,39 @@ import { EPerson } from 'src/app/shared/interface/person';
   styleUrl: './simple-datatable.component.css'
 })
 export class SimpleDatatableComponent {
-@Input() data: EPerson[];
+  @Input() data: EPerson[];
+
+  sortOrder = {
+    givenName: 'none',
+    surName: 'none',
+    age: 'none',
+    email: 'none',
+    education: 'none'
+  }
+
+  sortData(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc'){
+      this.sortOrder[sortKey] = 'desc'
+      this.data = sortBy(this.data, sortKey).reverse();
+    } else {
+      this.sortOrder[sortKey] = 'asc';
+      this.data = sortBy(this.data, sortKey);
+    }
+
+    for (let key in this.sortOrder) {
+      if (key !== sortKey) {
+        this.sortOrder[key] = 'none';
+      }
+    }
+  }
+
+  sortSign(sortKey: string) {
+    if (this.sortOrder[sortKey] === 'asc') {
+      return '↑';
+    } else if (this.sortOrder[sortKey] === 'desc') {
+      return '↓';
+    } else {
+      return '';
+    }
+  }
 }
