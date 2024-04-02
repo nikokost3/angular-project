984ead16ab35c62a83efcafd5ce7036368702e83
nikokost3 <nikoskos26@yahoo.gr>
2024-04-02 12:52:20 +0300

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
 import { Person } from './shared/interface/person';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersonTableComponent, EventBindExampleComponent],
+  imports: [RouterLink, RouterOutlet, PersonTableComponent, EventBindExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  name = 'Nikos';
 
   person0: Person = {
       givenName: 'Nikos',
